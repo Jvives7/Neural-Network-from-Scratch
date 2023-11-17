33362b95dcf8a674eaf207fc6b4919e4170071a4
Jvives7 <81063139+Jvives7@users.noreply.github.com>
2023-11-17 01:21:21 +0000

    commiting first revision

---
+++ helloworld.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+//using namespace std;
+
+class Neuron;
+
+typedef std::vector<Neuron> Layer; //used for aliasing existing data types, user-defined data types, and pointers to a more meaningful name. 
+                                    //Typedefs allow you to give descriptive names to standard data types, which can also help you self-document your code.
+
+
+
+class Net
+{
+public:
+// attributes of Net go here
+    Net(const std::vector<unsigned> topology); 
+    void feedForward(const std::vector<double>  &inputVals); // inputVals container is a vector of double type. pass the reference of input vals with&. Doesnt change anything in input vals argument so its a const.
+    void backProp(const std::vector<double>  &targetVals);
+    void getResults(std::vector<double>  &outputVals) const; // getResults will change the elements of the doubles. 
+private:
+    vector<Layer> m_layers; // m_layers[layerNum][neuronNum]
+};
+
+    Net::Net(const std::vector<unsigned> topology);{
+        //need to know how many layers of nn
+        unsigned numLayers = topology.size(); //.size member to get number of elements
+        for (unsigned layerNum = 0; layerNum)
+    }
+
+
+// main body
+int main(){
+    // How many layers and how many neurons per layer in net that we want.
+    // Topolgy will be a struct, class or obj type that we pass to the constructor.
+
+    // topology will send in unsigned integers to program the layer and neurons of net
+    // e.g., { 3, 2, 1 } 
+    std::vector<unsigned> topology; // unisgned vector is always non-negative
+    Net myNet(topology); // creates object myNet of Net class and passes the constructor 
+
+    
+
+    // During training program is interested in feedforward movement with inputs and
+    // backward propagation feedback movement with target values. 
+
+    //Feedforward job is to read values from input values and transfer to input neurons. Doesnt change anything in input vals argument so its a
+    std::vector<double> inputVals; // std:: namespace qualifier so compiler can find it. vector is a variable length array, needs atleast one parameter.
+    myNet.feedForward(inputVals); // access attributes and set values 
+
+    //Back propagation and tell the neurons what the result should have been or the Target value
+    std::vector<double> targetVals; // std:: namespace qualifier so compiler can find it. vector is a variable length array, needs atleast one parameter.
+    myNet.backProp(targetVals);
+
+    // get results
+    std::vector<double> outputVals;
+    myNet.getResults(outputVals); 
+}
